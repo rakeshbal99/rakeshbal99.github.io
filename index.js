4077c8f53b7b70faaa8e6252bcbb5bbb76898a5b
rakeshbal99 <rakeshbal99@gmail.com>
2026-01-11 23:59:39 -0800

    Fix: Default theme to dark mode for new visitors

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-  
+
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
+} else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    toggleSwitch.checked = true;
 }
 
 function switchTheme(e) {
